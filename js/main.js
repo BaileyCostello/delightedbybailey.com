5772af79f8a40c965229c1dcc694287f0ac513f3
Bailey Costello <bqacos@gmail.com>
2025-02-19 19:09:14 -0600

    Side scroll works!!!!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,42 @@
 /*javascript*/
+
+const stickySections = [...document.querySelectorAll('.sticky')]
+console.log(stickySections);
+
+let images = [
+  'assets/CDG Preview Image.png',
+  'assets/CDG Preview Image.png',
+  'assets/CDG Preview Image.png',
+  'assets/CDG Preview Image.png'
+]
+
+images.forEach(img => {
+  stickySections.forEach(section => {
+    let image = document.createElement('img');
+    image.src = img;
+    section.querySelector('.scroll_section').appendChild(image)
+  })
+})
+
+window.addEventListener('scroll', (e) => {
+  for(let i = 0; i < stickySections.length; i++) {
+    transform(stickySections[i])
+  }
+})
+
+function transform(section) {
+  const offsetTop = section.parentElement.offsetTop;
+  console.log(offsetTop);
+  const scrollSection = section.querySelector('.scroll_section')
+  console.log(scrollSection);
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+  console.log(percentage);
+  /*percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;*/
+  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
+
+}
+
+
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
